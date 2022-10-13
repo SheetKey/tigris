bc1ce67bda6bbc1639aafe99064a428ad71c7eec
Will <wveatchw@gmail.com>
2022-10-13 12:14:31 -0400

    window config

---
+++ src/Tigris/Graphics/Window.hs
@@ -0,0 +1,16 @@
+module Tigris.Graphics.Window where
+
+import qualified SDL
+
+windowConfig :: SDL.WindowConfig
+windowConfig = SDL.WindowConfig
+  { SDL.windowBorder          = True
+  , SDL.windowHighDPI         = False
+  , SDL.windowInputGrabbed    = False
+  , SDL.windowMode            = SDL.FullscreenDesktop
+  , SDL.windowGraphicsContext = SDL.NoGraphicsContext
+  , SDL.windowPosition        = SDL.Wherever
+  , SDL.windowResizable       = False
+  , SDL.windowInitialSize     = SDL.V2 800 600
+  , SDL.windowVisible         = True
+  }
