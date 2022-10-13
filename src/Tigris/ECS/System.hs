5e80844363f78aa2edae4d56133ff80127e94030
Will <wveatchw@gmail.com>
2022-10-13 17:51:46 -0400

    type synonyms

--- src/Tigris/ECS/System.hs
+++ src/Tigris/ECS/System.hs
@@ -17,3 +17,7 @@ type SNS m cl a b = SN (SystemT World m) cl a b
 type RhineS m cl a b = Rhine (SystemT World m) cl a b
 
 type SystemT' m a = SystemT World m a
+
+type ParClockS m clL clR = ParallelClock (SystemT World m) clL clR
+
+type SeqClockS m cl1 cl2 = ParallelClock (SystemT World m) cl1 cl2
