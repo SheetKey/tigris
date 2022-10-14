bba15602cbbf681ed28e10b4fc7b93be7e73899e
Will <wveatchw@gmail.com>
2022-10-14 10:54:57 -0400

    global tmvar store in stm

--- src/Tigris/ECS.hs
+++ src/Tigris/ECS.hs
@@ -6,3 +6,4 @@ import Tigris.ECS.World as X
 import Tigris.ECS.Components as X
 import Tigris.ECS.Process as X
 import Tigris.ECS.Clock as X
+import Tigris.ECS.Stores as X
---
+++ src/Tigris/ECS/Stores.hs
@@ -0,0 +1,6 @@
+module Tigris.ECS.Stores
+  ( module X
+  ) where
+
+-- mylib
+import Tigris.ECS.Stores.TMGlobal as X
