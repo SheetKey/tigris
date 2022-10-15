9c5f41b08830d5e12a6d9fb953b6dcda50c2c1d5
Will <wveatchw@gmail.com>
2022-10-15 16:53:11 -0400

    test

--- src/Tigris/Init.hs
+++ src/Tigris/Init.hs
@@ -29,6 +29,8 @@ initAndRun winName gameLoop = do
   SDLF.initialize
   win <- SDL.createWindow winName windowConfig
   ren <- SDL.createRenderer win (-1) SDL.defaultRenderer
+  p <- SDL.getAbsoluteMouseLocation
+  liftIO $ print p
   world <- initWorld
   runWith world $ do
     setReadOnly global $ Window win
