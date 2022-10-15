1fc41ede3b17ca1c7e6e4ba101fec817440da204
Will <wveatchw@gmail.com>
2022-10-15 11:04:13 -0400

    entity must have a destination to be rendered

--- src/Tigris/ECS/Process/Copy.hs
+++ src/Tigris/ECS/Process/Copy.hs
@@ -55,39 +55,39 @@ _copy = cmapM_ $ \(Texture texture) -> do
          Nothing
          (Just dest)
 
-  cmapM_ $ \(Position p, Not ::Not Destination, SpriteSheet {..}, Rotation {..}) -> do
-    Renderer ren <- get global
-    copyEx ren
-           texture
-           (Just (mkRect (colIndex * frameWidth) (rowIndex * frameHeight) frameWidth frameHeight))
-           (Just p)
-           angle
-           ((\(x, y) (Rectangle _ (V2 w h)) -> Just (P (V2 (w `div` x) (h `div` y)))) rotPntFrac p)
-           flipXY
+  --cmapM_ $ \(Position p, Not ::Not Destination, SpriteSheet {..}, Rotation {..}) -> do
+  --  Renderer ren <- get global
+  --  copyEx ren
+  --         texture
+  --         (Just (mkRect (colIndex * frameWidth) (rowIndex * frameHeight) frameWidth frameHeight))
+  --         (Just p)
+  --         angle
+  --         ((\(x, y) (Rectangle _ (V2 w h)) -> Just (P (V2 (w `div` x) (h `div` y)))) rotPntFrac p)
+  --         flipXY
 
-  cmapM_ $ \(Position p, Not ::Not Destination, SpriteSheet {..}, Not :: Not Rotation) -> do
-    Renderer ren <- get global
-    copy ren
-         texture
-         (Just (mkRect (colIndex * frameWidth) (rowIndex * frameHeight) frameWidth frameHeight))
-         (Just p)
+  --cmapM_ $ \(Position p, Not ::Not Destination, SpriteSheet {..}, Not :: Not Rotation) -> do
+  --  Renderer ren <- get global
+  --  copy ren
+  --       texture
+  --       (Just (mkRect (colIndex * frameWidth) (rowIndex * frameHeight) frameWidth frameHeight))
+  --       (Just p)
 
-  cmapM_ $ \(Position p, Not ::Not Destination, Not :: Not SpriteSheet, Rotation {..}) -> do
-    Renderer ren <- get global
-    copyEx ren
-           texture
-           Nothing
-           (Just p)
-           angle
-           ((\(x, y) (Rectangle _ (V2 w h)) -> Just (P (V2 (w `div` x) (h `div` y)))) rotPntFrac p)
-           flipXY
+  --cmapM_ $ \(Position p, Not ::Not Destination, Not :: Not SpriteSheet, Rotation {..}) -> do
+  --  Renderer ren <- get global
+  --  copyEx ren
+  --         texture
+  --         Nothing
+  --         (Just p)
+  --         angle
+  --         ((\(x, y) (Rectangle _ (V2 w h)) -> Just (P (V2 (w `div` x) (h `div` y)))) rotPntFrac p)
+  --         flipXY
 
-  cmapM_ $ \(Position p, Not ::Not Destination, Not :: Not SpriteSheet, Not :: Not Rotation) -> do
-    Renderer ren <- get global
-    copy ren
-         texture
-         Nothing
-         (Just p)
+  --cmapM_ $ \(Position p, Not ::Not Destination, Not :: Not SpriteSheet, Not :: Not Rotation) -> do
+  --  Renderer ren <- get global
+  --  copy ren
+  --       texture
+  --       Nothing
+  --       (Just p)
 
 copyAll :: MonadIO m => ClSFS m cl () ()
 copyAll = constMCl _copy
