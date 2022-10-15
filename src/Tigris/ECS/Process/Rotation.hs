6945ba1c40e910a92fff6453bb619bfb067b5210
Will <wveatchw@gmail.com>
2022-10-15 16:44:04 -0400

    rotation changes

--- src/Tigris/ECS/Process/Rotation.hs
+++ src/Tigris/ECS/Process/Rotation.hs
@@ -19,18 +19,23 @@ import Apecs
 -- sdl
 import qualified SDL
 
+_mousePosition :: MonadIO m => m (V2 CInt)
+_mousePosition = do
+  P v <- SDL.getAbsoluteMouseLocation
+  liftIO $ print v
+  return v
 
-mousePosition :: MonadIO m => ClSFS m cl () (Point V2 CInt)
-mousePosition = constMCl SDL.getAbsoluteMouseLocation >>> traceWith (liftIO . putStrLn) "Mouse position: "
+mousePosition :: MonadIO m => ClSFS m cl () (V2 CInt)
+mousePosition = constMCl _mousePosition
 
-calcAngle :: Point V2 CInt -> Rectangle CInt -> (CInt, CInt) -> CDouble
-calcAngle (P (V2 tx ty)) (Rectangle (P (V2 x y)) (V2 w h)) (xFrac, yFrac)
+calcAngle :: V2 CInt -> Rectangle CInt -> (CInt, CInt) -> CDouble
+calcAngle (V2 tx ty) (Rectangle (P (V2 x y)) (V2 w h)) (xFrac, yFrac)
   = let dx = fromIntegral (x - tx) + ((fromIntegral w) / (fromIntegral xFrac))
         dy = fromIntegral (y - ty) + ((fromIntegral h) / (fromIntegral yFrac))
         angle = ((atan2 dy dx) * 180 / 3.14) - 90
     in angle
 
-_rotateTowardsDest :: MonadIO m => Point V2 CInt -> SystemT' m ()
+_rotateTowardsDest :: MonadIO m => V2 CInt -> SystemT' m ()
 _rotateTowardsDest pnt = cmap $
   \(RToMouse, Rotation {..}, Destination dest) ->
     Rotation { angle = calcAngle pnt dest rotPntFrac, .. }
@@ -38,4 +43,4 @@ _rotateTowardsDest pnt = cmap $
 -- add a component to determine flipping rules
 
 rotateTowardsMouse :: MonadIO m => ClSFS m cl () ()
-rotateTowardsMouse = mousePosition >>> arrMCl _rotateTowardsDest >>> constMCl SDL.getMouseLocationMode >>> arrMCl (liftIO . putStrLn . show)
+rotateTowardsMouse = mousePosition >>> arrMCl _rotateTowardsDest
