de2d488cf6412fc3d5a4ddb710687e95d01ded21
Will <wveatchw@gmail.com>
2023-01-10 13:39:16 -0500

    generalize uv

--- src/Tigris/ECS/Process/UV.hs
+++ src/Tigris/ECS/Process/UV.hs
@@ -15,19 +15,34 @@ import Apecs
 -- linear
 import Linear
 
-_uv :: MonadIO m => SystemT' m ()
-_uv = cmap $ \(SpriteSheet {..}) -> 
+mkUV :: Int -> Int -> Int -> Int -> Int -> UV
+mkUV colIndex rowIndex frameWidth frameHeight borderWidth =
   let
     _tl = fmap fromIntegral $ V2 colIndex rowIndex
     _tr = fmap fromIntegral $ V2 (colIndex + frameWidth) rowIndex
     _br = fmap fromIntegral $ V2 (colIndex + frameWidth) (rowIndex - frameHeight)
     _bl = fmap fromIntegral $ V2 colIndex (rowIndex - frameHeight)
-    f = V2 (/ fromIntegral sheetWidth) (/ fromIntegral sheetHeight)
+    w = fromIntegral sheetWidth
+    h = fromIntegral sheetHeight
+    b = fromIntegral borderWidth
+    f = V2 (/ w) (/ h)
+    g = V4
+        (+ V2 (b / w) ((-b) / h))
+        (+ V2 ((-b) / w) ((-b) / h))
+        (+ V2 ((-b) / w) (b / h))
+        (+ V2 (b / w) (b / h))
     tl = f <*> _tl
     tr = f <*> _tr
     br = f <*> _br
     bl = f <*> _bl
-  in UV $ V4 tl tr br bl
+  in UV $ g <*> (V4 tl tr br bl)
+
+spriteToUV :: SpriteSheet -> UV
+spriteToUV SpriteSheet {..} = mkUV colIndex rowIndex frameWidth frameHeight borderWidth
+
+
+_uv :: MonadIO m => SystemT' m ()
+_uv = cmap spriteToUV
 
 uv :: MonadIO m => ClSFS m cl () ()
 uv = constMCl _uv
