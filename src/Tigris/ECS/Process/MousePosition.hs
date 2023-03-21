2190b70b660ab247e6a7ffe6da6dd10640597065
Will <wveatchw@gmail.com>
2023-03-21 11:43:07 -0400

    rework mouse position

--- src/Tigris/ECS/Process/MousePosition.hs
+++ src/Tigris/ECS/Process/MousePosition.hs
@@ -26,43 +26,28 @@ import Linear
 import GHC.Float (double2Float, float2Double)
 import Foreign.Marshal.Alloc
 import Foreign.Storable
+import Data.Int
+import Control.Monad ((>=>))
+import Foreign.C.Types
 
 
-_mousePosition :: MonadIO m => SystemT' m (V3 GL.GLfloat)
-_mousePosition = do
-  View view <- get global
-  Projection proj <- get global
-  view' :: GL.GLmatrix GL.GLdouble <- liftIO $ toMatrix $ ((fmap . fmap) float2Double) view
-  proj' :: GL.GLmatrix GL.GLdouble <- liftIO $ toMatrix $ ((fmap . fmap) float2Double) proj
-  model :: GL.GLmatrix GL.GLdouble <- liftIO $ toMatrix identity
-  vp <- GL.get GL.viewport
-  SDL.P (SDL.V2 mx my) <- SDL.getAbsoluteMouseLocation
-  GL.Vertex3 x y z <- liftIO $ GL.unProject
-                               (GL.Vertex3 (fromIntegral mx) (fromIntegral my) 0.6)
-                               view'
-                               proj'
-                               vp
-  return $ double2Float <$> (V3 x y z)
-
-mousePosition :: MonadIO m => ClSFS m cl () (V3 GL.GLfloat)
-mousePosition = constMCl _mousePosition
-
-_mousePosition' :: MonadIO m => m (V3 GL.GLfloat)
-_mousePosition' = do
-  SDL.P (SDL.V2 mx my) <- SDL.getAbsoluteMouseLocation
+_mouseZCoord :: MonadIO m => (Int32, Int32) -> SystemT' m (V3 GL.GLfloat)
+_mouseZCoord (mx, my) = do
+  Window win <- get global
+  V2 _ (CInt height) <- SDL.get $ SDL.windowSize win
   let winX = mx
-      winY = abs (600 - my)
+      winY = abs (height - my)
   z <- liftIO $ alloca $ \ptr -> do
     let pdata = GL.PixelData GL.DepthComponent GL.Float ptr
     GL.readPixels (GL.Position (fromIntegral winX) (fromIntegral winY)) (GL.Size 1 1) pdata
     peek ptr
   return $ V3 (fromIntegral winX) (fromIntegral winY) z
         
-mousePosition' :: MonadIO m => ClSFS m cl () (V3 GL.GLfloat)
-mousePosition' = constMCl _mousePosition'
+mouseZCoord :: MonadIO m => ClSFS m cl (Int32, Int32) (V3 GL.GLfloat)
+mouseZCoord = arrMCl _mouseZCoord
 
-_mouseCoord :: MonadIO m => V3 GL.GLfloat -> SystemT' m (V3 GL.GLfloat)
-_mouseCoord (V3 x y z) = do
+_screenToGameCoord :: MonadIO m => V3 GL.GLfloat -> SystemT' m (V3 GL.GLfloat)
+_screenToGameCoord (V3 x y z) = do
   View view <- get global
   Projection proj <- get global
   view' :: GL.GLmatrix GL.GLdouble <- liftIO $ toMatrix $ ((fmap . fmap) float2Double) view
@@ -76,5 +61,11 @@ _mouseCoord (V3 x y z) = do
                                vp
   return $ double2Float <$> (V3 x' y' z')
   
-mouseCoord :: MonadIO m => ClSFS m cl (V3 GL.GLfloat) (V3 GL.GLfloat)
-mouseCoord = arrMCl _mouseCoord
+screenToGameCoord :: MonadIO m => ClSFS m cl (V3 GL.GLfloat) (V3 GL.GLfloat)
+screenToGameCoord = arrMCl _screenToGameCoord
+
+_inGameMousePos :: MonadIO m => (Int32, Int32) -> SystemT' m (V3 GL.GLfloat)
+_inGameMousePos = _mouseZCoord >=> _screenToGameCoord
+
+inGameMousePos :: MonadIO m => ClSFS m cl (Int32, Int32) (V3 GL.GLfloat)
+inGameMousePos = arrMCl _inGameMousePos
