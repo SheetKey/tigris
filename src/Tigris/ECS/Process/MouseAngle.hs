578393106878e6ff71bde18e6f7f0d2e63842f8c
Will <wveatchw@gmail.com>
2023-03-29 11:28:14 -0400

    generic angle calculation

--- src/Tigris/ECS/Process/MouseAngle.hs
+++ src/Tigris/ECS/Process/MouseAngle.hs
@@ -3,6 +3,7 @@ module Tigris.ECS.Process.MouseAngle where
 -- tigris
 import Tigris.ECS.Components
 import Tigris.ECS.System
+import Tigris.ECS.Process.MousePosition
 
 -- apecs
 import Apecs
@@ -16,8 +17,14 @@ import qualified SDL
 -- opengl
 import qualified Graphics.Rendering.OpenGL as GL
 
+-- linear
+import Linear
 
 
+_angleFromTo :: V3 GL.GLfloat -> V3 GL.GLfloat -> GL.GLfloat
+_angleFromTo (V3 x _ z) (V3 mx _ mz) = atan2 (mz - z) (mx - x)
+
+-- dep
 _mouseAngle :: MonadIO m => SystemT' m GL.GLfloat
 _mouseAngle = do
   SDL.P (SDL.V2 mx my) <- SDL.getAbsoluteMouseLocation
@@ -28,5 +35,6 @@ _mouseAngle = do
       ang = atan2 ((fromIntegral my) - centerY) ((fromIntegral mx) - centerX)
   return ang
 
+-- dep
 mouseAngle :: MonadIO m => ClSFS m cl () GL.GLfloat
 mouseAngle = constMCl _mouseAngle
