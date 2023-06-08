7fcb8f282433d95ff29bf04acacd623d7e05474a
Will <wveatchw@gmail.com>
2023-06-08 16:18:26 -0400

    aabb overlaps

--- src/Tigris/Collision/AABB.hs
+++ src/Tigris/Collision/AABB.hs
@@ -23,6 +23,7 @@ class (Ord a, Num a, Eq (v a)) => BB v a where
   contains :: AABB v a -> AABB v a -> Bool
   area :: AABB v a -> a
   combine :: AABB v a -> AABB v a -> AABB v a
+  overlaps :: AABB v a -> AABB v a -> Bool
 
 instance (Ord a, Num a) => BB V3 a where
   nullAABB = AABB (V3 0 0 0) (V3 0 0 0)
@@ -43,6 +44,15 @@ instance (Ord a, Num a) => BB V3 a where
     = AABB
       (V3 (min lx1 lx2) (min ly1 ly2) (min lz1 lz2))
       (V3 (max ux1 ux2) (max uy1 uy2) (max uz1 uz2))
+  overlaps
+    (AABB (V3 lx1 ly1 lz1) (V3 ux1 uy1 uz1))
+    (AABB (V3 lx2 ly2 lz2) (V3 ux2 uy2 uz2))
+    = lx1 <= ux2
+    && ux1 >= lx2
+    && ly1 <= uy2
+    && uy1 >= ly2
+    && lz1 <= uz2
+    && uz1 >= lz2
 
 instance (Ord a, Num a) => BB V2 a where
   nullAABB = AABB (V2 0 0) (V2 0 0)
@@ -61,3 +71,10 @@ instance (Ord a, Num a) => BB V2 a where
     = AABB
       (V2 (min lx1 lx2) (min ly1 ly2))
       (V2 (max ux1 ux2) (max uy1 uy2))
+  overlaps
+    (AABB (V2 lx1 ly1) (V2 ux1 uy1))
+    (AABB (V2 lx2 ly2) (V2 ux2 uy2))
+    = lx1 <= ux2
+    && ux1 >= lx2
+    && ly1 <= uy2
+    && uy1 >= ly2
