c2869ee87817b58be97cf68291aa0f0c5ae586aa
Will <wveatchw@gmail.com>
2023-08-21 19:34:30 -0400

    temp fixes bc rhine 1.0 is broken

--- flake.nix
+++ flake.nix
@@ -2,21 +2,37 @@
   description = "A Haskell project template.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     rhine-unstable = {
       url = "github:turion/rhine";
       flake = false;
     };
+    
+    rhine-pinned = {
+      url = "github:turion/rhine/ed6476e4040417ab97ab3896dc8cdc312607c3d9";
+      flake = false;
+    };
+
+    vox-hs-in = {
+      url = "github:SheetKey/vox-hs";
+      flake = true;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rhine-unstable }: 
+  outputs = { self, nixpkgs, flake-utils, rhine-unstable, vox-hs-in, rhine-pinned }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        haskellPackages = pkgs.haskellPackages;
+        pkgs = nixpkgs.legacyPackages.${system}.extend (final: prev: {
+          config = prev.config // { allowBroken = true; };
+          haskellPackages = prev.haskellPackages.extend (hfinal: hprev: {
+            vox-hs = vox-hs-in.packages.${system}.vox-hs;
+          });
+        });
+        
+        # haskellPackages = pkgs.haskellPackages;
 
         packageName = "tigris";
 
@@ -25,7 +41,7 @@
 
       in {
         packages.${packageName} = # (ref:haskell-package-def)
-          haskellPackages.callCabal2nix packageName self rec {
+          pkgs.haskellPackages.callCabal2nix packageName self rec {
             # Dependency overrides go here
             # Ex with gi-gtk-declarative:
             # If version is broken then:
@@ -33,7 +49,10 @@
             # or if tests failing: 
             # gi-gtk-declarative = pkgs.haskell.lib.dontCheck haskellPackages.gi-gtk-declarative;
 
-            rhine = haskellPackages.callCabal2nix "rhine" (rhine-unstable + "/rhine") { };
+            # rhine = jailbreakUnbreak (pkgs.haskellPackages.callCabal2nix "rhine" (rhine-unstable + "/rhine") { });
+            rhine = jailbreakUnbreak pkgs.haskellPackages.rhine;
+            # rhine = jailbreakUnbreak (pkgs.haskellPackages.callCabal2nix "rhine" (rhine-pinned + "/rhine") {});
+            vox-hs = jailbreakUnbreak pkgs.haskellPackages.vox-hs;
           };
 
         defaultPackage = self.packages.${system}.${packageName};
